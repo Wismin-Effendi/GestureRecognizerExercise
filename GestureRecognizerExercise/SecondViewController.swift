b43be5acb55a00ce1937cd18a2cb37e0adf581f9
Wismin Effendi <wismin.effendi@gmail.com>
2017-06-06 16:58:47 -0500

    Add second controller with all Gesture implemented via Interface Builder

--- GestureRecognizerExercise/SecondViewController.swift
+++ GestureRecognizerExercise/SecondViewController.swift
@@ -1,8 +1,8 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  GestureRecognizerExercise
 //
-//   Gestures added programmatically
+//  Gestures added via  InterfaceBuilder  
 //
 //  Created by Wismin Effendi on 6/6/17.
 //  Copyright © 2017 iShinobi. All rights reserved.
@@ -10,7 +10,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var gestureLabel: UILabel!
+    
     
     enum Gesture: String {
         case tapGesture = "Tap Gesture"
@@ -22,9 +26,8 @@ class ViewController: UIViewController {
         case swipeDownGesture = "Swipe Down Gesture"
         case longPressGesture = "Long Press Gesture"
     }
-
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var gestureLabel: UILabel!
+    
+    var tapCounter: Int = 0
     
     var imageFilename: String? {
         didSet {
@@ -34,39 +37,13 @@ class ViewController: UIViewController {
         }
     }
     
-    var tapCounter: Int = 0
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setNewImage(for: "car")
-        setGestureText(for: nil)
-        
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        let rotateGesture = UIRotationGestureRecognizer(target: self, action: #selector(handleRotation(_:)))
-        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)))
-        
-        let swipeUpGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeUp(_:)))
-        swipeUpGesture.direction = .up
-
-        let swipeDownGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeDown(_:)))
-        swipeDownGesture.direction = .down
-        
-        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:)))
-        
-        let screenEdgePanGesture = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(handleScreenEdgePan(_:)))
-        screenEdgePanGesture.edges = .right
-        
-        
-        imageView.isUserInteractionEnabled = true
-        imageView.gestureRecognizers = [panGesture, tapGesture, rotateGesture, pinchGesture, longPressGesture]
-        
-        // swipe up and swipe down + screen edge are to be added to the root View
-        view.gestureRecognizers = [swipeUpGesture, swipeDownGesture, screenEdgePanGesture]
     }
 
-    // helper 
+    
+    // helper
     private func setNewImage(for basename: String) {
         let idx = arc4random_uniform(10)
         let imageFileName = basename + "0\(idx).jpg"
@@ -84,21 +61,21 @@ class ViewController: UIViewController {
     
     
     // Gesture handler
-    func handleTap(_ sender: UITapGestureRecognizer) {
+    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
         tapCounter += 1
         
         // reset to initial size & no roation
         if let view = sender.view {
             view.transform = CGAffineTransform.identity
         }
-        // change to different train / car / motorcycle 
+        // change to different train / car / motorcycle
         let tapVehicleType = ["car", "motorcycle", "train"]
         let choosenVehicleType = tapVehicleType[tapCounter % 3]
         setNewImage(for: choosenVehicleType)
         setGestureText(for:.tapGesture)
     }
     
-    func handlePinch(_ sender: UIPinchGestureRecognizer) {
+    @IBAction  func handlePinch(_ sender: UIPinchGestureRecognizer) {
         // zoom in / out
         if let view = sender.view {
             view.transform = view.transform.scaledBy(x: sender.scale, y: sender.scale)
@@ -107,7 +84,7 @@ class ViewController: UIViewController {
         setGestureText(for: .pinchGesture)
     }
     
-    func handlePan(_ sender: UIPanGestureRecognizer) {
+    @IBAction  func handlePan(_ sender: UIPanGestureRecognizer) {
         // move the image
         let translation = sender.translation(in: self.view)
         if let view = sender.view {
@@ -118,14 +95,14 @@ class ViewController: UIViewController {
         setGestureText(for: .panGesture)
     }
     
-    func handleScreenEdgePan(_ sender: UIScreenEdgePanGestureRecognizer) {
+    @IBAction  func handleScreenEdgePan(_ sender: UIScreenEdgePanGestureRecognizer) {
         // change opacity of the background
         let alpha = sender.location(in: view).x / view.bounds.width
-        view.alpha = alpha 
+        view.alpha = alpha
         setGestureText(for: .screenEdgePanGesture)
     }
     
-    func handleRotation(_ sender: UIRotationGestureRecognizer) {
+    @IBAction func handleRotation(_ sender: UIRotationGestureRecognizer) {
         // rotate image
         if let view = sender.view {
             view.transform = view.transform.rotated(by: sender.rotation)
@@ -134,20 +111,20 @@ class ViewController: UIViewController {
         setGestureText(for: .rotationGesture)
     }
     
-    func handleSwipeUp(_ sender: UISwipeGestureRecognizer) {
+    @IBAction func handleSwipeUp(_ sender: UISwipeGestureRecognizer) {
         // change image to a random airplane
         setNewImage(for: "airplane")
         setGestureText(for: .swipeUpGesture)
     }
     
-    func handleSwipeDown(_ sender: UISwipeGestureRecognizer) {
+    @IBAction func handleSwipeDown(_ sender: UISwipeGestureRecognizer) {
         // change image to a random airplane
         setNewImage(for: "ship")
         setGestureText(for: .swipeDownGesture)
     }
     
-    func handleLongPress(_ sender: UILongPressGestureRecognizer) {
-        // reset to initial size & no roation 
+    @IBAction func handleLongPress(_ sender: UILongPressGestureRecognizer) {
+        // reset to initial size & no roation
         if let view = sender.view {
             view.transform = CGAffineTransform.identity
         }
@@ -155,6 +132,8 @@ class ViewController: UIViewController {
         setNewImage(for: "bicycle")
         setGestureText(for: .longPressGesture)
     }
+    
+
+
 
 }
-
